4952288889fd35b2959f336c596166e1e00789b8
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2023-08-24 13:14:05 -0300

    Commit contra o Lacerda

--- conta.js
+++ conta.js
@@ -15,6 +15,10 @@ class ContaBancaria {
         this.saldo = this.saldo - saque;
         return " Meu novo saldo é " + "R$" + this.saldo + " após o saque";
     }
+    novoSaque(saque) {
+        this.saldo = this.saldo - saque;
+        return " Meu novo saldo é " + "R$" + this.saldo + " após o saque";
+    }
 }
 let minhaConta = new ContaBancaria("João", 1000);
 console.log(minhaConta.consultar());
@@ -22,3 +26,21 @@ console.log(minhaConta.depositar(500));
 console.log(minhaConta.consultar());
 console.log(minhaConta.sacar(200));
 console.log(minhaConta.consultar());
+console.log(minhaConta.novoSaque(1500));
+console.log(minhaConta.consultar());
+let minhaConta2 = new ContaBancaria("Mariazinha244", 10000);
+console.log(minhaConta2.consultar());
+console.log(minhaConta2.depositar(500));
+console.log(minhaConta2.consultar());
+console.log(minhaConta2.sacar(200));
+console.log(minhaConta2.consultar());
+console.log(minhaConta2.novoSaque(1500));
+console.log(minhaConta2.consultar());
+let minhaConta3 = new ContaBancaria("Claudinhão", 9000);
+console.log(minhaConta3.consultar());
+console.log(minhaConta3.depositar(500));
+console.log(minhaConta3.consultar());
+console.log(minhaConta3.sacar(200));
+console.log(minhaConta3.consultar());
+console.log(minhaConta3.novoSaque(1500));
+console.log(minhaConta3.consultar());
