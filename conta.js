30b4f5bd29c9052735030d8369452bb0adcaeae8
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2023-08-24 13:05:55 -0300

    Commit de segurança

---
+++ conta.js
@@ -0,0 +1,24 @@
+"use strict";
+class ContaBancaria {
+    constructor(nome, saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+    depositar(deposito) {
+        this.saldo = this.saldo + deposito;
+        return " E meu novo saldo é " + "R$" + this.saldo + " após o depósito";
+    }
+    consultar() {
+        return "Meu nome é " + this.nome + " e eu tenho " + "R$" + this.saldo + " na minha conta";
+    }
+    sacar(saque) {
+        this.saldo = this.saldo - saque;
+        return " Meu novo saldo é " + "R$" + this.saldo + " após o saque";
+    }
+}
+let minhaConta = new ContaBancaria("João", 1000);
+console.log(minhaConta.consultar());
+console.log(minhaConta.depositar(500));
+console.log(minhaConta.consultar());
+console.log(minhaConta.sacar(200));
+console.log(minhaConta.consultar());
